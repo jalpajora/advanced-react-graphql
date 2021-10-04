c3068fad04a61ab3bc5081c88577bb73a705a53e
jalpajora <jezamae.alpajora@gmail.com>
2021-10-04 21:50:36 +0800

    feat: module 2 - nextjs

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import Header from './Header';
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  children: PropTypes.oneOf([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
